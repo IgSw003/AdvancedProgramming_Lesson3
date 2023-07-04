2cb2be14d04307ff72f65f1767ec01ebd2c5dba0
IgorSwiderek <Igor.Swiderek.priv@gmail.com>
2023-07-04 20:56:56 +0200

    Dodaj pliki projektów.

---
+++ AdvancedProgramming_Lesson3/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace AdvancedProgramming_Lesson3.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Genre { get; set; }
+
+        public string ISBN { get; set; }
+    }
+}
